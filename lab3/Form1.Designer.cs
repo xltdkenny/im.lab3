dd5880a6d10852360b1f3d75fe1bd4e7292d8399
kotov <kotov@M1ROSHA1>
2024-03-08 23:12:46 +0700

    Добавьте файлы проекта.

---
+++ lab3/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace lab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btStart = new System.Windows.Forms.Button();
+            this.edRule = new System.Windows.Forms.NumericUpDown();
+            this.rule = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.table = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.edRule)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.table)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.panel1.Controls.Add(this.btStart);
+            this.panel1.Controls.Add(this.edRule);
+            this.panel1.Controls.Add(this.rule);
+            this.panel1.Location = new System.Drawing.Point(12, 19);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(235, 465);
+            this.panel1.TabIndex = 0;
+            // 
+            // btStart
+            // 
+            this.btStart.Location = new System.Drawing.Point(59, 59);
+            this.btStart.Name = "btStart";
+            this.btStart.Size = new System.Drawing.Size(120, 27);
+            this.btStart.TabIndex = 2;
+            this.btStart.Text = "Start";
+            this.btStart.UseVisualStyleBackColor = true;
+            this.btStart.Click += new System.EventHandler(this.btStart_Click);
+            // 
+            // edRule
+            // 
+            this.edRule.Location = new System.Drawing.Point(59, 33);
+            this.edRule.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.edRule.Name = "edRule";
+            this.edRule.Size = new System.Drawing.Size(120, 20);
+            this.edRule.TabIndex = 6;
+            this.edRule.Value = new decimal(new int[] {
+            110,
+            0,
+            0,
+            0});
+            // 
+            // rule
+            // 
+            this.rule.AutoSize = true;
+            this.rule.Location = new System.Drawing.Point(102, 17);
+            this.rule.Name = "rule";
+            this.rule.Size = new System.Drawing.Size(32, 13);
+            this.rule.TabIndex = 0;
+            this.rule.Text = "Rule:";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // table
+            // 
+            this.table.AllowUserToOrderColumns = true;
+            this.table.BackgroundColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.table.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.table.Location = new System.Drawing.Point(253, 19);
+            this.table.Name = "table";
+            this.table.Size = new System.Drawing.Size(446, 465);
+            this.table.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(727, 508);
+            this.Controls.Add(this.table);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.edRule)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.table)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btStart;
+        private System.Windows.Forms.NumericUpDown edRule;
+        private System.Windows.Forms.Label rule;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.DataGridView table;
+    }
+}
+
---
+++ lab3/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab3
+{
+    public partial class Form1 : Form
+    {
+        bool help = true;
+        string biNumber;
+        List<int> FirstLine;
+        List<int> line = new List<int>(new int[20]);
+        List<int> otherLine = new List<int>(new int[20]);
+
+        int NumCells = 20;
+        Random rnd = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeTable();
+        }
+
+        private void InitializeTable()
+        {
+            table.RowCount = NumCells;
+            table.ColumnCount = NumCells;
+            for (int i = 0; i < NumCells; i++)
+            {
+                table.Columns[i].Width = 20;
+                table.Rows[i].Cells[0].Value = i;
+            }
+        }
+
+        private void InitializeFirstLine()
+        {
+            FirstLine = new List<int>(new int[NumCells]);
+            for (int i = 1; i < FirstLine.Count; i++)
+            {
+                int k = rnd.Next(0, 2);
+                if (k == 1)
+                {
+                    FirstLine[i] = 1;
+                    table.Rows[0].Cells[i].Style.BackColor = Color.LightBlue;
+                }
+                else
+                    FirstLine[i] = 0;
+            }
+        }
+
+        private void btStart_Click(object sender, EventArgs e)
+        {
+
+            if (help)
+            {
+                table.Refresh();
+
+                biNumber = Convert.ToString((int)edRule.Value, 2).PadLeft(8, '0');
+
+                if (biNumber.Length < 8)
+                {
+                    int n = 8 - biNumber.Length;
+                    for (int i = 0; i < n; i++) biNumber = biNumber.Insert(0, "0");
+                }
+
+                InitializeFirstLine();
+
+                for (int i = 0; i < otherLine.Count; i++) otherLine[i] = 0;
+
+                table.Refresh();
+                line = FirstLine;
+            }
+
+            help = false;
+
+            if (btStart.Text == "Start")
+            {
+                btStart.Text = "Stop";
+                timer1.Start();
+            }
+            else
+            {
+                btStart.Text = "Start";
+                timer1.Stop();
+            }
+            table.Refresh();
+        }
+
+        public int check(string help)
+        {
+            switch (help)
+            {
+                case "111":
+                    return biNumber[0] - 48;
+                case "110":
+                    return biNumber[1] - 48;
+                case "101":
+                    return biNumber[2] - 48;
+                case "100":
+                    return biNumber[3] - 48;
+                case "011":
+                    return biNumber[4] - 48;
+                case "010":
+                    return biNumber[5] - 48;
+                case "001":
+                    return biNumber[6] - 48;
+                case "000":
+                    return biNumber[7] - 48;
+                default:
+                    return biNumber[0] - 48;
+            }
+        }
+
+        int iter = 1;
+        bool First = true;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (First == true)
+            {
+                First = false;
+            }
+            else
+            {
+                for (int i = 0; i < otherLine.Count; i++)
+                {
+                    line[i] = otherLine[i];
+                }
+            }
+
+            for (int i = 0; i < line.Count; i++)
+            {
+                if (i != 0 && i != line.Count - 1)
+                {
+                    string Result = "";
+                    Result = line[i - 1].ToString() + line[i].ToString() + line[i + 1].ToString();
+                    int result = check(Result);
+                    otherLine[i] = result;
+                    if (otherLine[i] == 1)
+                    {
+                        table.Rows[iter].Cells[i].Style.BackColor = Color.LightBlue;
+                    }
+                }
+            }
+
+            table.Refresh();
+            iter++;
+
+            if (iter == 20)
+            {
+                timer1.Stop();
+            }
+            
+        }
+    }
+}
